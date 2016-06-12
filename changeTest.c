3ce94a65f377a348e2bb2a933d48290e30fb0e34
Konstantin Pryanichnikov <kean.dev@gmail.com>
2016-06-12 14:08:00 +0000

    2

--- changeTest.c
+++ changeTest.c
@@ -7,19 +7,31 @@ int main(void)
 {
      
     printf("Vvedite, pozhaluysta chislo v formate 1.24 \n"); 
-    int giveChange = GetFloat();
-    giveChange = giveChange * 100; //translate grivny v kopeiki
+    int changeToGive = GetFloat(); //sdachu vidat' monetami
+  
+    float tselaya, drobnaya; 
+    drobnaya =  modff(changeToGive, &tselaya); //vydelyaem tselyu i drobnuyu chast
+     
+    
+    printf("Tselaya 123  chast' %lf \n", tselaya);
+    printf("Kopeiki chast' %lf \n", drobnaya);
+    
+    //tselaya chast, tselaya chast * 100
+    //drobnaya chast
+    
+    
+    return 0;
     int currentNominalMonety = 25;
     int coinCount = 0;      //kolichestvo monet
     //int divChange = 0;
     int sdachaOstalas = 0; 
    ///Tselim deleniem proveryaem
-    sdachaOstalas = giveChange / currentNominalMonety;  //proveryaem vhodit li znachene monety v obschee kolichestvo sdachi
+    sdachaOstalas = changeToGive / currentNominalMonety;  //proveryaem vhodit li znachene monety v obschee kolichestvo sdachi
   //divChange skolko monet vsyali,
-    coinCount = giveChange % currentNominalMonety; //esli vhodit znachenie monetki v obschee kolvo, to berem ostatok
+    coinCount = changeToGive % currentNominalMonety; //esli vhodit znachenie monetki v obschee kolvo, to berem ostatok
    
     printf("kolichestvo monet ostalos ot obschego kolichestva : %d \n ", coinCount);
     printf("vsyali monet currentNominala : %d \n ", sdachaOstalas);
     
-    return 0;  
+      
 }
